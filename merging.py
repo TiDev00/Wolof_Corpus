fa694a75fbb9678e327db3c7a9cf9db9207f36c9
TiDev00 <cisse.thierno@outlook.com>
2022-09-30 01:00:42 -0400

    num of lines

--- merging.py
+++ merging.py
@@ -5,18 +5,22 @@ import glob
 def all_text_in_one(directory):
     list_of_files = sorted(filter(os.path.isfile, glob.glob(directory + '/*')))
     for file_path in list_of_files:
-        line_count = 0
+        with open(file_path, 'r', encoding='utf-8') as input_file:
+            with open('bible_wol.txt', 'a') as output_file:
+                output_file.seek(0)
+                for line in input_file:
+                    output_file.write(line)
+
+
+def number_of_line(directory):
+    list_of_files = sorted(filter(os.path.isfile, glob.glob(directory + '/*')))
+    for file_path in list_of_files:
+        count_line = 0
         for line in file_path:
-            line_count += 1
-        print("{0} = {1}".format(file_path, line_count))
-        # with open(file_path, 'r', encoding='utf-8') as input_file:
-        #     with open('bible_wol.txt', 'a') as output_file:
-        #         output_file.seek(0)
-        #         for line in input_file:
-        #             output_file.write(line)
+            count_line += 1
+        print("{0} = {1}".format(file_path, count_line))
 
-
+# number_of_line('src_lang/frasbl_readaloud')
+# number_of_line('src_lang/wolKYG_readaloud')
 # all_text_in_one('src_lang/frasbl_readaloud')
-
-
-all_text_in_one('src_lang/wolKYG_readaloud')
+# all_text_in_one('src_lang/wolKYG_readaloud')
