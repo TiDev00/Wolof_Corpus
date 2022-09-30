463ec7b63cc21abb4985bd08b1860523378f01cc
TiDev00 <cisse.thierno@outlook.com>
2022-09-29 22:44:25 -0400

    merging function by order

---
+++ merging.py
@@ -0,0 +1,12 @@
+import os
+import glob
+
+
+def all_text_in_one(directory):
+    list_of_files = sorted(filter(os.path.isfile, glob.glob(directory + '*')))
+    for file_path in list_of_files:
+        with open(file_path, 'r', encoding='utf-8') as input_file:
+            with open('bible_fr.txt', 'a') as output_file:
+                output_file.seek(0)
+                for line in input_file:
+                    output_file.write(line)
