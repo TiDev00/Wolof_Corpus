0edc31b50c94c110c4b773df1334fab55b467411
TiDev00 <cisse.thierno@outlook.com>
2022-09-29 23:58:31 -0400

    update files

--- merging.py
+++ merging.py
@@ -5,14 +5,18 @@ import glob
 def all_text_in_one(directory):
     list_of_files = sorted(filter(os.path.isfile, glob.glob(directory + '/*')))
     for file_path in list_of_files:
-        with open(file_path, 'r', encoding='utf-8') as input_file:
-            with open('bible_fr.txt', 'a') as output_file:
-                output_file.seek(0)
-                for line in input_file:
-                    output_file.write(line)
+        line_count = 0
+        for line in file_path:
+            line_count += 1
+        print("{0} = {1}".format(file_path, line_count))
+        # with open(file_path, 'r', encoding='utf-8') as input_file:
+        #     with open('bible_wol.txt', 'a') as output_file:
+        #         output_file.seek(0)
+        #         for line in input_file:
+        #             output_file.write(line)
 
 
-all_text_in_one('src_lang/frasbl_readaloud')
+# all_text_in_one('src_lang/frasbl_readaloud')
 
 
 all_text_in_one('src_lang/wolKYG_readaloud')
