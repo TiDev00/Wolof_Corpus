224af04b192354732a143a3f50b8b3375c5819d4
TiDev00 <cisse.thierno@outlook.com>
2022-11-01 18:25:51 -0400

    alignement bible fr et wol

--- processing/merging.py
+++ processing/merging.py
@@ -12,12 +12,12 @@ def get_encoding(directory):
 def merge_txt(directory, file_name):
     for file_path in glob.glob(directory + '/*.txt'):
         with open(file_path, 'r', encoding='utf-8-sig') as input_file:
-            with open("text_scrapped/bible/" + file_name, 'a') as output_file:
+            with open("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped" + file_name, 'a', encoding='utf-8-sig') as output_file:
                 for line in input_file:
                     output_file.write(line)
 
 
 # get_encoding("text_scrapped/bible/frasbl_readaloud")
 # get_encoding("text_scrapped/bible/wolKYG_readaloud")
-merge_txt("text_scrapped/bible/frasbl_readaloud", "bible_fr.txt")
-merge_txt("text_scrapped/bible/wolKYG_readaloud", "bible_wol.txt")
+merge_txt("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped/bible/frasbl_readaloud", "/bible_fr.txt")
+merge_txt("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped/bible/wolKYG_readaloud", "/bible_wol.txt")
