906686eca9e87323f44a04c3077e98861f3c94c5
TiDev00 <cisse.thierno@outlook.com>
2022-11-02 20:23:55 -0400

    line number verification start 1SA

--- processing/merging.py
+++ processing/merging.py
@@ -2,22 +2,29 @@ import glob
 import chardet
 
 
+def num_line(directory):
+    for file_path in sorted(glob.glob(directory + '/*')):
+        with open(file_path, 'r') as filename:
+            print("{0} = {1}".format(file_path, len(filename.readlines())))
+
+
 def get_encoding(directory):
-    for filename in glob.glob(directory + "/*.txt"):
+    for filename in sorted(glob.glob(directory + "/*.txt")):
         with open(filename, 'rb') as rawdata:
             result = chardet.detect(rawdata.read())
         print(filename.ljust(45), result['encoding'])
 
 
 def merge_txt(directory, file_name):
-    for file_path in glob.glob(directory + '/*.txt'):
+    for file_path in sorted(glob.glob(directory + '/*.txt')):
         with open(file_path, 'r', encoding='utf-8-sig') as input_file:
             with open("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped" + file_name, 'a', encoding='utf-8-sig') as output_file:
                 for line in input_file:
                     output_file.write(line)
 
-
+# num_line("text_scrapped/bible/frasbl_readaloud")
+# num_line("text_scrapped/bible/wolKYG_readaloud")
 # get_encoding("text_scrapped/bible/frasbl_readaloud")
 # get_encoding("text_scrapped/bible/wolKYG_readaloud")
-merge_txt("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped/bible/frasbl_readaloud", "/bible_fr.txt")
-merge_txt("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped/bible/wolKYG_readaloud", "/bible_wol.txt")
+# merge_txt("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped/bible/frasbl_readaloud", "/bible_fr.txt")
+# merge_txt("C:/Users/ee591997/Desktop/Wolof_Corpus/text_scrapped/bible/wolKYG_readaloud", "/bible_wol.txt")
