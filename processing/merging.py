0ecb024de904d8362c33c03e8be722b41f3c492a
TiDev00 <cisse.thierno@outlook.com>
2022-11-11 23:02:18 -0500

    avant rm numb coran

--- processing/merging.py
+++ processing/merging.py
@@ -39,4 +39,4 @@ def xml_processing(directory1):
 # get_encoding("text_scrapped/bible/txt/wolKYG_readaloud")
 # merge_txt("text_scrapped/bible/frasbl_readaloud", "/bible_fr.txt")
 # merge_txt("text_scrapped/bible/wolKYG_readaloud", "/bible_wol.txt")
-xml_processing("text_scrapped/bible/vpl/frasbl_vpl/frasbl_vpl.xml")
+# xml_processing("text_scrapped/bible/vpl/frasbl_vpl/frasbl_vpl.xml")
--- processing/splitting.py
+++ processing/splitting.py
@@ -2,17 +2,6 @@ from pathlib import Path
 import re
 from sentence_splitter import SentenceSplitter
 
-
-def read_file(directory):
-    text = " "
-    with open(directory, 'r', encoding='utf-8-sig') as input_file:
-        for line in input_file:
-            re.sub('\n', ' ', line)
-            re.sub(' ', ' ', line)
-            text = text + ' ' + line
-        print(text)
-
-
 # read_file("C:/Users/mkdiallo/Desktop/004_LEV_14_read.txt")
 # Object interface
 splitter = SentenceSplitter(language='fr')
@@ -24,3 +13,4 @@ print(splitter.split(text=""""""))
 #     text='This is a paragraph. It contains several sentences. "But why," you ask?',
 #     language='en'
 # ))
+
