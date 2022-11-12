40088fc6d020d60a7fbc61825ee653fff98d0d73
TiDev00 <cisse.thierno@outlook.com>
2022-11-12 14:14:03 -0500

    numb start rm for coran verse fr et wol

--- processing/splitting.py
+++ processing/splitting.py
@@ -1,16 +1,10 @@
+# -*- coding: utf-8 -*-
 from pathlib import Path
 import re
 from sentence_splitter import SentenceSplitter
 
-# read_file("C:/Users/mkdiallo/Desktop/004_LEV_14_read.txt")
+
 # Object interface
 splitter = SentenceSplitter(language='fr')
-print(splitter.split(text=""""""))
-
-
-# Functional interface
-# print(split_text_into_sentences(
-#     text='This is a paragraph. It contains several sentences. "But why," you ask?',
-#     language='en'
-# ))
+print(len(splitter.split(text="""""")))
 
