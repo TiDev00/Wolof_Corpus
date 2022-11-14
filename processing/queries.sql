c907188e2d2fb692937e2a5116821a6df0f939b2
TiDev00 <cisse.thierno@outlook.com>
2022-11-14 18:28:59 -0500

    splitting impl

--- processing/queries.sql
+++ processing/queries.sql
@@ -1,36 +1,26 @@
-# # Select all rows in bible_fr table
-# SELECT * FROM fr_bib;
-#
-# # Select all rows in bible_wol table
-# SELECT * FROM wol_bib;
-#
-# # Select all verses containig other verses in bible_wol table
-# SELECT * FROM wol_bib WHERE wol_bib.startVerse != wol_bib.endVerse;
-#
-# # Select all verses containig other verses in bible_fr table
-# SELECT * FROM fr_bib WHERE fr_bib.startVerse != fr_bib.endVerse;
-#
-# # Select verses in bible_wol table and not in bible_fr table
-# SELECT * FROM wol_bib WHERE wol_bib.canon_order NOT IN (SELECT fr_bib.canon_order FROM fr_bib);
+# Select all rows in bible_fr table
+SELECT * FROM fr_bib;
 
-# # Select verses in bible_fr table and not in bible_wol table
-# SELECT * FROM fr_bib WHERE fr_bib.canon_order NOT IN (SELECT wol_bib.canon_order FROM wol_bib);
+# Select all rows in bible_wol table
+SELECT * FROM wol_bib;
 
-# # delete verse in bible_fr table and not bible_wol table
-# DELETE FROM fr_bib WHERE fr_bib.canon_order NOT IN (SELECT wol_bib.canon_order FROM wol_bib);
+# Select all verses containing other verses in bible_wol table
+SELECT * FROM wol_bib WHERE wol_bib.startVerse != wol_bib.endVerse;
 
-# # Select and order by canon_order, chapter and startverse
-# SELECT * FROM fr_bib ORDER BY canon_order, chapter, startVerse
+# Select all verses containing other verses in bible_fr table
+SELECT * FROM fr_bib WHERE fr_bib.startVerse != fr_bib.endVerse;
 
-# delete underscore contained in canon_order column in bible_fr table
-# UPDATE bible.fr_bib SET canon_order = REPLACE(canon_order,'_','');
+# Select verses in bible_wol table and not in bible_fr table
+SELECT * FROM wol_bib WHERE wol_bib.canon_order NOT IN (SELECT fr_bib.canon_order FROM fr_bib);
 
-# # delete underscore contained in canon_order column in bible_wol table
-# UPDATE bible.wol_bib SET canon_order = REPLACE(canon_order,'_','');
-#
-# # Modify canon_order column datatype in bible_fr table
-# ALTER TABLE bible.fr_bib MODIFY COLUMN canon_order INT(12);
-#
-# # Modify canon_order column datatype in bible_wol table
-# ALTER TABLE bible.wol_bib MODIFY COLUMN canon_order INT(12);
+# Select verses in bible_fr table and not in bible_wol table
+SELECT * FROM fr_bib WHERE fr_bib.canon_order NOT IN (SELECT wol_bib.canon_order FROM wol_bib);
 
+# delete verse in bible_fr table and not in bible_wol table
+DELETE FROM fr_bib WHERE fr_bib.canon_order NOT IN (SELECT wol_bib.canon_order FROM wol_bib);
+
+# delete leading and trailing \n in verseText fr_bib.verseText column
+UPDATE bible.fr_bib SET verseText = TRIM('\n' FROM verseText);
+
+# delete leading and trailing \n in verseText wol_bib.verseText column
+UPDATE bible.fr_bib SET verseText = TRIM('\n' FROM verseText);
--- processing/splitting.py
+++ processing/splitting.py
@@ -1,11 +1,70 @@
 # -*- coding: utf-8 -*-
-from pathlib import Path
+
+import nltk
 import re
+import spacy
 from sentence_splitter import SentenceSplitter
 
 
-# Object interface
-splitter = SentenceSplitter(language='fr')
-print(len(splitter.split(text="""""")))
+def nltk_splitting(filepath):
+    nltk.download('punkt')
+    fp = open(filepath)
+    data = fp.read()
+    for line in data:
+        line = line.replace("\n", "")
+    return nltk.tokenize.sent_tokenize(data, 'french')
 
 
+def spacy_splitting(filepath):
+    fp = open(filepath)
+    data = fp.read()
+    nlp = spacy.load('fr')
+    tokens = nlp(data)
+    for sent in tokens.sents:
+        return sent.string.strip()
+
+
+def regex_splitting(filepath):
+    alphabets = "([A-Za-z])"
+    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+    websites = "[.](com|net|org|io|gov)"
+    digits = "([0-9])"
+
+    text = " " + filepath + "  "
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1", text)
+    text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
+    if "..." in text: text = text.replace("...", "<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>", text)
+    text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+    text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text: text = text.replace(".”", "”.")
+    if "\"" in text: text = text.replace(".\"", "\".")
+    if "!" in text: text = text.replace("!\"", "\"!")
+    if "?" in text: text = text.replace("?\"", "\"?")
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+
+# # Object interface
+# splitter = SentenceSplitter(language='fr')
+# print(len(splitter.split(text="""""")))
+
+if __name__ == "__main__":
+    file = "../text_scrapped/coran/echantillon.txt"
+    print(nltk_splitting(file))
