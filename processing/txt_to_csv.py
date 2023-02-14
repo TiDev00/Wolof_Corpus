9fb180b273496c066581b4fbb9271715ab3891fe
TiDev00 <cisse.thierno@outlook.com>
2023-02-13 23:25:07 -0500

    coran txt to csv

---
+++ processing/txt_to_csv.py
@@ -0,0 +1,20 @@
+def convertor(filepath, output_filepath):
+    data = open(filepath).readlines()
+
+    sourates_list = []
+
+    for verses in data:
+        sourates_list.append((verses.rstrip().split('|')[0], verses.rstrip().split('|')[1]))
+
+    sourates_dict = {}
+    for key, values in sourates_list:
+        sourates_dict.setdefault(key, []).append(values)
+
+    with open(output_filepath, 'w') as f:
+        for sourate, verses in sourates_dict.items():
+            for verse in verses:
+                f.write("{},{},{},\"{}\"\n".format(list(sourates_dict).index(sourate) + 1, sourate,
+                                             verses.index(verse) + 1, verse))
+
+
+convertor('../text_scrapped/religious/coran/fr/coran_fr.txt', 'test.csv')
