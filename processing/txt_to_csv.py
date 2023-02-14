5df03ab7cc423c79c196bcc05e09f609cd9073fb
TiDev00 <cisse.thierno@outlook.com>
2023-02-13 23:34:07 -0500

    coran csv wol et fr

--- processing/txt_to_csv.py
+++ processing/txt_to_csv.py
@@ -11,10 +11,12 @@ def convertor(filepath, output_filepath):
         sourates_dict.setdefault(key, []).append(values)
 
     with open(output_filepath, 'w') as f:
+        f.write("{},{},{},{}\n".format('SourateID', 'Sourate', 'VerseID', 'Verse'))
         for sourate, verses in sourates_dict.items():
             for verse in verses:
                 f.write("{},{},{},\"{}\"\n".format(list(sourates_dict).index(sourate) + 1, sourate,
-                                             verses.index(verse) + 1, verse))
+                                                   verses.index(verse) + 1, verse))
 
 
-convertor('../text_scrapped/religious/coran/fr/coran_fr.txt', 'test.csv')
+convertor('../text_scrapped/religious/coran/fr/coran_fr.txt', '../text_scrapped/religious/coran/fr/coran_fr.csv')
+convertor('../text_scrapped/religious/coran/wol/coran_wol.txt', '../text_scrapped/religious/coran/wol/coran_wol.csv')
