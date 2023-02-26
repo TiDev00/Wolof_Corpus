45b85da8e99b97996ca78998a41138f3300bdf0b
TiDev00 <cisse.thierno@outlook.com>
2023-02-26 01:04:26 -0500

    coran csv

--- processing/csv_processings.py
+++ processing/csv_processings.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-df_wol_coran = pd.read_csv('../text_scrapped/religious/coran/fr/fr_cor.csv')
-print(df_wol_coran)
+df_fr_cor = pd.read_csv('../text_scrapped/religious/coran/fr/fr_cor.csv')
+df_wol_cor = pd.read_csv('../text_scrapped/religious/coran/wol/wol_cor.csv')
+
+print(df_fr_cor.head(5))
+print(df_wol_cor.head(5))
