2af101a7369f29e93266aa37e4ff62ffa9dfb14d
TiDev00 <cisse.thierno@outlook.com>
2023-02-27 03:11:49 -0500

    score suppression after alignment and splittings

---
+++ processing/aligned_processing.py
@@ -0,0 +1,21 @@
+from csv_processings import output_generation
+
+
+def score_suppression(filepath):
+    data = open(filepath).readlines()
+    fr_sentences = []
+    wol_sentences = []
+    for line in data:
+        fr_sentences.append(line.split('\t')[0])
+        wol_sentences.append(line.split('\t')[1])
+    return fr_sentences, wol_sentences
+
+
+if __name__ == '__main__':
+    cor_file = score_suppression('../text_scrapped/religious/coran/aligned_cor.txt')
+    bib_file = score_suppression('../text_scrapped/religious/bible/aligned_bib.txt')
+
+    output_generation(cor_file[0], '../text_scrapped/religious/coran/aligned_fr_cor.txt')
+    output_generation(cor_file[1], '../text_scrapped/religious/coran/aligned_wol_cor.txt')
+    output_generation(bib_file[0], '../text_scrapped/religious/bible/aligned_fr_bib.txt')
+    output_generation(bib_file[1], '../text_scrapped/religious/bible/aligned_wol_bib.txt')
